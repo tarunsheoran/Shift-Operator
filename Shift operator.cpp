8e1aa6c68c5357e9b594724b1712c4f7b87d7e1d
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-06 21:18:01 +0530

    Add files via upload

---
+++ Shift operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Right shift = "<<(13>>1)<<endl;
+	cout<<"Right shift = "<<(18>>2)<<endl;
+	cout<<"Left shift = "<<(13<<1)<<endl;
+	cout<<"Lest shift = "<<(13<<3)<<endl;
+
+
+} 
